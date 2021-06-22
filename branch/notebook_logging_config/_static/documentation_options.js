6509348259f42c12485ed6b96de4979990d3ad02
DhruvSondhi <DhruvSondhi@users.noreply.github.com>
2021-06-22 16:02:36 +0000

    deploy: 9629195d1fe42bae44803e9e810437a296ccf5c1

--- branch/notebook_logging_config/_static/documentation_options.js
+++ branch/notebook_logging_config/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.dev4034',
+    VERSION: '3.0.dev4036',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
