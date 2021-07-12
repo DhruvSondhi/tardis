ebe213a5704b9b2e8fa3402a0914f4f717844d09
Dhruv Sondhi <dhruvsondhi05@gmail.com>
2021-07-12 19:30:41 +0530

    Added more debug messages for the simulation progress

--- tardis/montecarlo/montecarlo_numba/base.py
+++ tardis/montecarlo/montecarlo_numba/base.py
@@ -27,8 +27,11 @@ from tardis.montecarlo.montecarlo_numba.single_packet_loop import (
 from tardis.montecarlo.montecarlo_numba import njit_dict
 from numba.typed import List
 
+logger = logging.getLogger(__name__)
+
 
 def montecarlo_radial1d(model, plasma, runner):
+    logger.debug("Configuring the Packet Collection for Montecarlo Main Loop")
     packet_collection = PacketCollection(
         runner.input_r,
         runner.input_nu,
@@ -38,22 +41,35 @@ def montecarlo_radial1d(model, plasma, runner):
         runner._output_energy,
     )
 
+    logger.debug("Setting the Numba Model Parameters for the Run")
     numba_model = NumbaModel(
         runner.r_inner_cgs,
         runner.r_outer_cgs,
         model.time_explosion.to("s").value,
     )
+    logger.debug("Initializing the Numba Plasma Model for Simulation run")
     numba_plasma = numba_plasma_initialize(plasma, runner.line_interaction_type)
+    logger.debug(
+        "Setting up the Values for the Estimator Arrays :\n\t[j_estimator, nu_bar_estimator, j_blue_estimator, Edotlu_estimator]"
+    )
     estimators = Estimators(
         runner.j_estimator,
         runner.nu_bar_estimator,
         runner.j_blue_estimator,
         runner.Edotlu_estimator,
     )
+
     packet_seeds = montecarlo_configuration.packet_seeds
-
+    logger.debug(
+        f"Packet Seeds for the Simulation : {montecarlo_configuration.packet_seeds}\n\tNumber Of Packet Seeds = {len(montecarlo_configuration.packet_seeds)}"
+    )
     number_of_vpackets = montecarlo_configuration.number_of_vpackets
+    logger.debug(f"Number Of Virtual Packets considered = {number_of_vpackets}")
 
+    logger.debug("Running the Main Loop for the Simulation")
+    logger.debug(
+        "This is a NJIT (Numba Just In Time) complied Function,\n\tFor Detailed logs about the Montecarlo Main Loop, please configure the Montecarlo Numba Logger\n"
+    )
     (
         v_packets_energy_hist,
         last_interaction_type,
@@ -79,36 +95,62 @@ def montecarlo_radial1d(model, plasma, runner):
     )
 
     runner._montecarlo_virtual_luminosity.value[:] = v_packets_energy_hist
+    logger.debug(
+        f"Montecarlo Virtual Luminosity Values : {runner._montecarlo_virtual_luminosity.value[:]}"
+    )
     runner.last_interaction_type = last_interaction_type
+    logger.debug(f"Last Interaction Type : {runner.last_interaction_type}")
     runner.last_interaction_in_nu = last_interaction_in_nu
+    logger.debug(f"Last Interaction in nu : {runner.last_interaction_in_nu}")
     runner.last_line_interaction_in_id = last_line_interaction_in_id
+    logger.debug(
+        f"Last Line Interaction In Id : {runner.last_line_interaction_in_id}"
+    )
     runner.last_line_interaction_out_id = last_line_interaction_out_id
+    logger.debug(
+        f"Last Line Interaction Out Id : {runner.last_line_interaction_out_id}"
+    )
 
     if montecarlo_configuration.VPACKET_LOGGING and number_of_vpackets > 0:
+        # Log message To be determined correctly
+        logger.debug(
+            "Virtual Packet Logging Turned ON\n\tDetails for Virtual Packet Logging"
+        )
         runner.virt_packet_nus = np.concatenate(
-            np.array(virt_packet_nus)
+            np.array(
+                virt_packet_nus,
+                dtype=object,
+            ),
         ).ravel()
+        logger.debug(f"Virtual Packet nu's = {runner.virt_packet_nus}")
         runner.virt_packet_energies = np.concatenate(
-            np.array(virt_packet_energies)
+            np.array(virt_packet_energies, dtype=object),
         ).ravel()
-        runner.virt_packet_initial_mus = np.concatenate(
-            np.array(virt_packet_initial_mus)
-        ).ravel()
-        runner.virt_packet_initial_rs = np.concatenate(
-            np.array(virt_packet_initial_rs)
-        ).ravel()
+        logger.debug(f"Virtual Packet Energies = {runner.virt_packet_nus}")
         runner.virt_packet_last_interaction_in_nu = np.concatenate(
-            np.array(virt_packet_last_interaction_in_nu)
+            np.array(virt_packet_last_interaction_in_nu, dtype=object),
         ).ravel()
+        logger.debug(
+            f"Virtual Packet Last Interaction in nu = {runner.virt_packet_nus}"
+        )
         runner.virt_packet_last_interaction_type = np.concatenate(
-            np.array(virt_packet_last_interaction_type)
+            np.array(virt_packet_last_interaction_type, dtype=object),
         ).ravel()
+        logger.debug(
+            f"Virtual Packet Last Interaction Type = {runner.virt_packet_nus}"
+        )
         runner.virt_packet_last_line_interaction_in_id = np.concatenate(
-            np.array(virt_packet_last_line_interaction_in_id)
+            np.array(virt_packet_last_line_interaction_in_id, dtype=object),
         ).ravel()
+        logger.debug(
+            f"Virtual Packet Last Line Interaction In Id = {runner.virt_packet_nus}"
+        )
         runner.virt_packet_last_line_interaction_out_id = np.concatenate(
-            np.array(virt_packet_last_line_interaction_out_id)
+            np.array(virt_packet_last_line_interaction_out_id, dtype=object),
         ).ravel()
+        logger.debug(
+            f"Virtual Packet Last Line Interaction In Id = {runner.virt_packet_nus}"
+        )
 
 
 @njit(**njit_dict)
@@ -214,8 +256,12 @@ def montecarlo_main_loop(
 
         vpackets_nu = vpacket_collection.nus[: vpacket_collection.idx]
         vpackets_energy = vpacket_collection.energies[: vpacket_collection.idx]
-        vpackets_initial_mu = vpacket_collection.initial_mus[: vpacket_collection.idx]
-        vpackets_initial_r = vpacket_collection.initial_rs[: vpacket_collection.idx]
+        vpackets_initial_mu = vpacket_collection.initial_mus[
+            : vpacket_collection.idx
+        ]
+        vpackets_initial_r = vpacket_collection.initial_rs[
+            : vpacket_collection.idx
+        ]
 
         v_packets_idx = np.floor(
             (vpackets_nu - spectrum_frequency[0]) / delta_nu
@@ -233,17 +279,29 @@ def montecarlo_main_loop(
     if montecarlo_configuration.VPACKET_LOGGING:
         for vpacket_collection in vpacket_collections:
             vpackets_nu = vpacket_collection.nus[: vpacket_collection.idx]
-            vpackets_energy = vpacket_collection.energies[: vpacket_collection.idx]
-            vpackets_initial_mu = vpacket_collection.initial_mus[: vpacket_collection.idx]
-            vpackets_initial_r = vpacket_collection.initial_rs[: vpacket_collection.idx]
+            vpackets_energy = vpacket_collection.energies[
+                : vpacket_collection.idx
+            ]
+            vpackets_initial_mu = vpacket_collection.initial_mus[
+                : vpacket_collection.idx
+            ]
+            vpackets_initial_r = vpacket_collection.initial_rs[
+                : vpacket_collection.idx
+            ]
             virt_packet_nus.append(np.ascontiguousarray(vpackets_nu))
             virt_packet_energies.append(np.ascontiguousarray(vpackets_energy))
-            virt_packet_initial_mus.append(np.ascontiguousarray(vpackets_initial_mu))
-            virt_packet_initial_rs.append(np.ascontiguousarray(vpackets_initial_r))
-            virt_packet_last_interaction_in_nu.append(np.ascontiguousarray(
-                vpacket_collection.last_interaction_in_nu[
-                    : vpacket_collection.idx
-                ])
+            virt_packet_initial_mus.append(
+                np.ascontiguousarray(vpackets_initial_mu)
+            )
+            virt_packet_initial_rs.append(
+                np.ascontiguousarray(vpackets_initial_r)
+            )
+            virt_packet_last_interaction_in_nu.append(
+                np.ascontiguousarray(
+                    vpacket_collection.last_interaction_in_nu[
+                        : vpacket_collection.idx
+                    ]
+                )
             )
             virt_packet_last_interaction_type.append(
                 np.ascontiguousarray(
